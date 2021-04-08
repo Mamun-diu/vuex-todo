b26197e7efea133a03f9ed9197f5fafd65356361
Mamun <mamun511510782@gmail.com>
2021-04-08 08:32:31 +0600

    Complete vuex-todo app

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,14 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    margin : 0;
+    padding : 0;
+    box-sizing: border-box;
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  }
+  body{
+    font-family: sans-serif;
+    background: #c8ebdf;
+  }
 </style>
